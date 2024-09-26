b550e90d087e33c01b53d413893beecd3ace1a69
Felipe Gómez <felipekan17@gmail.com>
2024-09-26 17:01:51 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+function App() {
+  const [totalSpent, setTotalSpent] = useState(0)
+
+  useEffect(() => {
+    async function fetchTotal() {
+      const res = await fetch('api/expenses/total-spent')
+      const data = await res.json()
+      setTotalSpent(data.total)
+    }
+    fetchTotal()
+  }, [])
+
+  return (
+    <Card className="w-[350px] m-auto">
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>The total amount you have spent</CardDescription>
+      </CardHeader>
+      <CardContent>{totalSpent}</CardContent>
+    </Card>
+  )
+}
+
+export default App
---
+++ server/routes/expenses.ts
@@ -0,0 +1,48 @@
+import { Hono } from 'hono'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+const fakeExpenses: Expense[] = [
+  { id: 1, title: 'Groceries', amount: 50 },
+  { id: 2, title: 'Utilities', amount: 100 },
+  { id: 3, title: 'Rent', amount: 1000 },
+]
+
+const expenseSchema = z.object({
+  id: z.number().int().positive(),
+  title: z.string().min(2).max(100),
+  amount: z.number().int().positive(),
+})
+
+type Expense = z.infer<typeof expenseSchema>
+
+const createPostSchema = expenseSchema.omit({ id: true })
+
+export const expensesRoute = new Hono()
+  .get('/', async (c) => {
+    return c.json({ expenses: fakeExpenses })
+  })
+  .post('/', zValidator('json', createPostSchema), async (c) => {
+    const expense = await c.req.valid('json')
+
+    fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 })
+    c.status(201)
+    return c.json(expense)
+  })
+  .get('/total-spent', (c) => {
+    const total = fakeExpenses.reduce((acc, expense) => acc + expense.amount, 0)
+    return c.json({ total })
+  })
+  .get('/:id{[0-9]+}', (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const expense = fakeExpenses.find((expense) => expense.id === id)
+    if (!expense) return c.notFound()
+    return c.json({ expense })
+  })
+  .delete('/:id{[0-9]+}', (c) => {
+    const id = Number.parseInt(c.req.param('id'))
+    const index = fakeExpenses.findIndex((expense) => expense.id === id)
+    if (index === -1) return c.notFound()
+    const deletedExpense = fakeExpenses.splice(index, 1)[0]
+    return c.json({ deletedExpense })
+  })
